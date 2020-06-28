92415626e56941abb107b1dcd55cf2c95cd17d57
zy <2652510143@qq.com>
2020-06-28 14:53:21 +0800

    friendmapper

---
+++ src/main/java/com/weblog/demo/demo/mapper/zymapper/UserFriMapper.java
@@ -0,0 +1,29 @@
+package com.weblog.demo.demo.mapper.zymapper;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface UserFriMapper {
+    //找出该id的所有好友
+    @Select("select * from user_friends where user_id = #{id}")
+    public List<Map<String,Object>> findFriByid (int id);
+
+    //该表有id和user_id两个表项，这里用到userid
+    @Insert("insert into user_friends(user_id,user_friends_id,user_note,user_status)" +
+            "value(#{user.id},#{user.friendsid},#{user.note}," +
+            "#{user.status})")
+    public int save(@Param("user") Map<String,String> map);
+
+    //用户id和好友id来删除id
+    @Delete("delete from user_friends where user_id =#{uid} and user_friends_id =#{fid}")
+    public int delete(@Param("uid") int userid,@Param("fid") int friid);
+
+    @Update("update user_friends " +
+            " set user_id=#{user.id},user_friends_id=#{user.friendsid}," +
+            "user_note=#{user.note},user_status=#{user.status}" +
+            " where user_id=#{user.id} and user_friends_id =#{user.friendsid}")
+    public int update(@Param("user") Map<String,String> user);
+}
---
+++ src/test/java/com/weblog/demo/demo/mapper/zymapper/UserFriMapperTest.java
@@ -0,0 +1,52 @@
+package com.weblog.demo.demo.mapper.zymapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+class UserFriMapperTest {
+    @Resource
+    private UserFriMapper userFriMapper;
+    @Test
+    void findFriByid() {
+    }
+
+    @Test
+    void save() {
+        //user_id,user_friends_id,user_note,user_status
+        Map<String,String> map=new HashMap<>();
+        map.put("id","2");
+        map.put("friendsid","1");
+        map.put("note","你才是mdzz");
+        map.put("status","1");
+
+        int n=userFriMapper.save(map);
+        System.out.println(n);
+    }
+
+    @Test
+    void delete() {
+        int n=userFriMapper.delete(2,3);
+        System.out.println(n);
+
+    }
+
+    @Test
+    void update() {
+        Map<String,String> map=new HashMap<>();
+        map.put("id","1");
+        map.put("friendsid","2");
+        map.put("note","nmsl");
+        map.put("status","0");
+        int n=userFriMapper.update(map);
+        System.out.println(n);
+    }
+}

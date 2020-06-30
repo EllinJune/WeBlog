c4bc7608e7c0139629d5f1272120f1bc49d69850
丁俊颖 <767225082@qq.com>
2020-06-30 17:16:38 +0800

    ArticleMapper

--- src/main/java/com/weblog/demo/demo/mapper/djymapper/ArticleMapper.java
+++ src/main/java/com/weblog/demo/demo/mapper/djymapper/ArticleMapper.java
@@ -1,13 +1,16 @@
 package com.weblog.demo.demo.mapper.djymapper;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface ArticleMapper
 {
+    @Select("select article_title,article_content,article_like_count,article_comment_count,article_collection_count,article_date from articles")
+    public List<Map<String,Object>> message();
     @Select("select * from articles where article_id=#{id}")
-    public Map<String,Object> findById(int id);
+    public boolean findById(int id);
     //写文章
     @Insert("insert into articles(article_title,article_content)" +
             "value(#{article.title},#{article.content})")
@@ -21,6 +24,7 @@ public interface ArticleMapper
             " set article_title=#{t.title},article_content=#{t.content} " +
             " where article_id=#{t.id}")
     public int updateArticle(@Param("t") Map<String,String> t);
+    //删除文章
     @Delete("delete from articles where article_id=#{id}")
     public int delete(int id);
 }
